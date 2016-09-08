780906c1a46f594afa19e651168cf702774d630a
Paulo Porto <newpaulinhu@gmail.com>
2016-09-07 22:33:48 -0300

    projeto

---
+++ src/main/java/br/com/braille/portal/BraillePortalApplication.java
@@ -0,0 +1,12 @@
+package br.com.braille.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BraillePortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BraillePortalApplication.class, args);
+	}
+}
